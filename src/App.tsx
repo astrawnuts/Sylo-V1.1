61d901ff2d2ae5070d5beec9461f9f62aef37229
astrawnuts <astrawnuts@gmail.com>
2021-08-02 22:33:59 +0530

    added pages and components -- added a startscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.scss";
+import StartScreen from "./pages/StartScreen";
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <img src="landingImage.png" alt="" />
+      <StartScreen />
     </div>
   );
 }
---
+++ src/pages/StartScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function StartScreen() {
+  return (
+    <>
+      <h1>hello world from start screen</h1>
+    </>
+  );
+}
