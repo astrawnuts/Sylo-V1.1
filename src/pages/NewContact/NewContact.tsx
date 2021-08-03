da04503057c5b2b2a612be1afd315e6d7946c964
astrawnuts <astrawnuts@gmail.com>
2021-08-03 23:26:18 +0530

    new contact page done visually

--- src/pages/NewContact/NewContact.tsx
+++ src/pages/NewContact/NewContact.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react";
 import PreviousPage from "../../components/PreviousPage/PreviousPage";
+import "./NewContact.scss";
 
 interface Props { }
 
@@ -8,10 +9,16 @@ interface Props { }
 export default function NewContact({ }: Props): ReactElement {
 
     return (
-
         <div>
             <PreviousPage HeaderTitle="New Contact" to="/contacts" />
-            
+
+            <form action="" className="NewContact">
+                <input type="text" className="input" placeholder="Name" />
+                <input type="text" className="input" placeholder="Ethereum address" />
+                <p className="NewContact__error">Please enter a valid Ethereum address</p>
+
+                <a type="button" className="button button__orange">Save</a>
+            </form>
         </div>
 
     );
