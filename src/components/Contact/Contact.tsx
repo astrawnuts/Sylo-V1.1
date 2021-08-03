016277c4a7723cf56af2db39f9ace3ccdb26c3ba
astrawnuts <astrawnuts@gmail.com>
2021-08-03 20:45:22 +0530

    added the individual contact screen we can now render this

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,14 +1,29 @@
 import React, { ReactElement } from 'react'
 import "./Contact.scss";
 
-interface Props {
-    
+interface ContactProps {
+    name: String, 
 }
 
-export default function Contact({}: Props): ReactElement {
+export default function Contact({name}: ContactProps): ReactElement {
+    function hi({name}: ContactProps) {
+        const myArr = name.split(" ");
+        let str:string = "";
+        myArr.forEach(element => {
+            str += element.substring(0,1).toUpperCase() + " ";
+        });
+
+        return str;
+
+    }
+
+
     return (
-        <div>
-            This is a contact
+        <div className="contact">
+            <div className="contact__row">
+                <div className="contact__photo">{hi({name})}</div>
+                <div className="contact__name">{name}</div>
+            </div>
         </div>
     )
 }
--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -6,15 +6,53 @@ import Contact from "../../components/Contact/Contact";
 
 interface Props {}
 
+interface Person {
+  name: string, 
+  etherID: string,
+}
+
 // To handle all things contacts related
 export default function Contacts({}: Props): ReactElement {
+
+  //initialising an empty people array of type Person
+  const people:Person[] = [];
+
+
+  //first check if there are any people in local storage
+  const storedPeople:string = JSON.parse(localStorage.getItem("people") || '{}');
+
+  //if there are you need to loop over them and draw them in the view
+
+  //else show a message like no contacts in database
+
+  //allow person to create new contacts and store them to local storage
+
+ 
+  people[0] = { 
+    name:"Tom", 
+    etherID:"Hanks" 
+ }; 
+
+ people[1] = { 
+  name:"Bill", 
+  etherID:"Trainer" 
+}; 
+
+
+ localStorage.setItem("people", JSON.stringify(people));
+
+
+
   return (
     <div>
       <div>
      
         <PreviousPage HeaderTitle="Address Book" to="/" />
 
-        <Contact />
+        <Contact name="Steve Bronson" />
+        <Contact name="Steve Jobs" />
+        <Contact name="Harrison Ford Lettuce" />
+        <Contact name="Harriot Turner" />
       </div>
       <Link className="button button__blue" to="/" type="button">Disconnect</Link>
     </div>
